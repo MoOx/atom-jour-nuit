a4117b15e462992025dde02bf7740356f25d301b
Maxime Thirouin <m@moox.io>
2016-02-12 15:19:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Atom f.lux
+
+[![Build status](https://img.shields.io/travis/MoOx/atom-f.lux/master.svg?branch=master&label=build)](https://travis-ci.org/MoOx/atom-f.lux)
+
+> Like F.lux, for Atom themes.
+
+This package auto switch UI/Syntax themes depending on the time of the day.
+
+Install this package, then **go to the package setting to choose your prefered hours and select the themes you want to use.**
+
+---
+
+## CONTRIBUTING
+
+* ⇄ Pull requests and ★ Stars are always welcome.
+* For bugs and feature requests, please create an issue.
+* Pull requests must be accompanied by passing automated tests (`$ npm test`).
+
+## [CHANGELOG](CHANGELOG.md)
+
+## [LICENSE](LICENSE)
---
+++ spec/index-spec.js
@@ -0,0 +1,68 @@
+/** @babel */
+
+import { schema } from "../lib/config"
+import flux from "../lib/index"
+
+const noTheme = [ "", "" ]
+const getThemes = () => atom.config.get("core.themes")
+
+describe("atom f.lux", () => {
+  beforeEach(() => {
+    spyOn(Date, "now")
+    window.setInterval = window.fakeSetInterval
+    atom.config.set("core.themes", noTheme)
+  })
+
+  const initTest = (h) => {
+    Date.now.andReturn(new Date(null, null, null, h).getTime())
+    flux.activate()
+  }
+
+  it("should change again at night", () => {
+    runs(() => initTest(schema.sunset.default))
+    waitsFor(
+      () => getThemes()[0] != "",
+      "atom didn't change theme (" + getThemes() + ")"
+    )
+    runs(() =>
+      expect(getThemes())
+      .toEqual([ schema.nightUITheme.default, schema.nightSyntaxTheme.default ])
+    )
+  })
+
+  it("should change again at day light", () => {
+    runs(() => initTest(schema.sunrise.default))
+    waitsFor(
+      () => getThemes()[0] != "",
+      "atom didn't change theme (" + getThemes() + ")"
+    )
+    runs(() =>
+      expect(getThemes())
+      .toEqual([ schema.dayUITheme.default, schema.daySyntaxTheme.default ])
+    )
+  })
+
+  it("should change at night", () => {
+    runs(() => initTest(23))
+    waitsFor(
+      () => getThemes()[0] != "",
+      "atom didn't change theme (" + getThemes() + ")"
+    )
+    runs(() =>
+      expect(getThemes())
+      .toEqual([ schema.nightUITheme.default, schema.nightSyntaxTheme.default ])
+    )
+  })
+
+  it("should change at day light", () => {
+    runs(() => initTest(10))
+    waitsFor(
+      () => getThemes()[0] != "",
+      "atom didn't change theme (" + getThemes() + ")"
+    )
+    runs(() =>
+      expect(getThemes())
+      .toEqual([ schema.dayUITheme.default, schema.daySyntaxTheme.default ])
+    )
+  })
+})
